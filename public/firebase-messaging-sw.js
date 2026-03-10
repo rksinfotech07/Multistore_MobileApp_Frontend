768d5ce067c0979264d0d7589006f6a141e918a2
rksinfotech07 <rksinfotech07@gmail.com>
2026-03-10 15:49:55 +0530

    Fcm Get done 1

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,11 +2,11 @@ importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js")
 importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "XXXX",
-  authDomain: "XXXX",
-  projectId: "XXXX",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyC3hAIqhkKsTsmQ13Jva8p2SPQzQE9noZM",
+  authDomain: "mabzo-fcm.firebaseapp.com",
+  projectId: "mabzo-fcm",
+  messagingSenderId: "568526845616",
+  appId: "1:568526845616:web:a1d0025e33f40d50c2ed75"
 });
 
 const messaging = firebase.messaging();
--- src/utils/getFcmToken.js
+++ src/utils/getFcmToken.js
@@ -4,7 +4,7 @@ import { messaging } from "../firebase";
 export const getFcmToken = async () => {
   try {
     const token = await getToken(messaging, {
-      vapidKey: "YOUR_VAPID_KEY"
+      vapidKey: "BGO9XsTz-bXbdK7lU-YXMIYiro7eGhrwf70-2xHTUimmueo74HLlRsrHEVDIG3FUNZHLPdtWw1Bl0G3Np21a8YM"
     });
 
     console.log("FCM Token:", token);
