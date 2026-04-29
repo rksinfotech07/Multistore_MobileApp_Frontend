ff92c1c8bb84da66e0f34fbdd771c1f54b8e596b
infotech-stack <rksinfotech3@gmail.com>
2026-04-29 16:00:52 +0530

    resolution for netlify

--- src/firebase.js
+++ src/firebase.js
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3hAIqhkKsTsmQ13Jva8p2SPQzQE9noZM",
-  authDomain: "mabzo-fcm.firebaseapp.com",
-  projectId: "mabzo-fcm",
-  messagingSenderId: "568526845616",
-  appId: "1:568526845616:web:a1d0025e33f40d50c2ed75"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
